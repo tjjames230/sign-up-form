b86ca6822a0d9d8959493750cf68dd5f7244869e
tjjames230 <108950500+tjjames230@users.noreply.github.com>
2023-11-03 14:26:20 -0500

    added logic to check passwords match

--- script/app.js
+++ script/app.js
@@ -1,10 +1,33 @@
 const submitBtn = document.querySelector("button");
 const password = document.querySelector("#password");
 const confirmPassword = document.querySelector("#confirm-password");
+const main = document.querySelector("main");
+let p = document.createElement("p"); // Create the <p> element.
 
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
 
-  if (password.value === confirmPassword.value) {
+  if (main.contains(p)) {
+    main.removeChild(p);
   }
+
+  if (password.value === confirmPassword.value) {
+    password.style.outline = "none";
+    password.style.border = "1px solid hsl(143, 89%, 48%)";
+    confirmPassword.style.outline = "none";
+    confirmPassword.style.border = "1px solid hsl(143, 89%, 48%)";
+
+    p.innerText = "Passwords match! Congrats!";
+    p.style.color = "green";
+  } else {
+    password.style.outline = "none";
+    password.style.border = "1px solid hsl(0, 89%, 48%)";
+    confirmPassword.style.outline = "none";
+    confirmPassword.style.border = "1px solid hsl(0, 89%, 48%)";
+
+    p.innerText = "Passwords must match in order to submit.";
+    p.style.color = "red";
+  }
+
+  main.appendChild(p); // Append the <p> element to the main element.
 });
